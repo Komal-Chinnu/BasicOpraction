0e70883ade1686b682bd202ffb04b7400b403728
Komal-Chinnu <komalpawar2k13@gmail.com>
2025-05-06 10:18:51 +0530

    saved

---
+++ DivisionOperaction.java
@@ -0,0 +1,18 @@
+package BasicOP;
+
+public class DivisionOperaction {
+
+    public static void main(String[] args) {
+        
+        int a = 15;
+        int b = 0;
+        try {
+            int div = a / b;
+            System.out.println("Division :" +div);
+        }
+        catch(ArithmeticException e){
+            System.out.println("Error : Cannot divide by Zero");
+        }
+    }
+
+}
